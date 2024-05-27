2528a3c3cccb5463c28c640348205da687872b71
Adedapo Aderemi <adedapopaul@yahoo.com>
2024-05-27 13:28:37 +0100

    improve search descriptions

--- controllers/blog/searchBlogs.js
+++ controllers/blog/searchBlogs.js
@@ -24,23 +24,24 @@ exports.main = async (kwargs) => {
         )
       )
       .then(async (results) => {
-
         const iso3 = await results.map((p) => p.iso3);
-        const countries = await DB.general.any(
-          `
+        const countries = await DB.general
+          .any(
+            `
               SELECT name,  iso3
               FROM country_names 
               WHERE iso3 = ANY($1) 
           `,
-          [iso3]
-        ).catch(err=>{
-          console.log(err)
-          return []
-        });
+            [iso3]
+          )
+          .catch((err) => {
+            console.log(err);
+            return [];
+          });
 
         const data = await results.map((p) => ({
           ...p,
-          country: countries.find(k=> k.iso3 == p.iso3)?.name || '',
+          country: countries.find((k) => k.iso3 == p.iso3)?.name || "",
           date: convertToDate(p),
           matched_texts: extractMatchedTexts(p, terms),
         }));
@@ -63,14 +64,16 @@ exports.main = async (kwargs) => {
 };
 
 const extractMatchedTexts = (searchResults, searchWords) => {
-  const textToSearch = `${searchResults.title} ${searchResults.content}`;
+  const textToSearch = removeNavigationTexts(
+    `${searchResults.title || ""} ${searchResults.content || ""}`
+  );
 
   if (searchWords.length === 0) {
     const startWordIndex = textToSearch.substring(100, 200).split(/\s+/).length;
     const endWordIndex = startWordIndex + 50;
     const words = textToSearch.split(/\s+/).slice(startWordIndex, endWordIndex);
     // return words.join(" ");
-    return ''
+    return "";
   }
 
   const matchedTexts = [];
@@ -80,12 +83,7 @@ const extractMatchedTexts = (searchResults, searchWords) => {
     const match = regex.exec(textToSearch);
 
     if (match) {
-      const startWordIndex =
-        textToSearch.substring(0, match.index).split(/\s+/).length - 26;
-      const endWordIndex = startWordIndex + 50;
-      const words = textToSearch
-        .split(/\s+/)
-        .slice(startWordIndex, endWordIndex);
+      const words = match.input.split(/\s+/).slice(0, 50);
       const matchedText = words.join(" ");
       matchedTexts.push(matchedText);
       if (matchedTexts.length) return matchedTexts.join("");
@@ -93,9 +91,29 @@ const extractMatchedTexts = (searchResults, searchWords) => {
   }
 
   if (matchedTexts.length === 0) {
-    const startWordIndex = textToSearch.split(/\s+/).length - 26;
-    const endWordIndex = startWordIndex + 50;
-    const words = textToSearch.split(/\s+/).slice(startWordIndex, endWordIndex);
+    const textToSearch = removeNavigationTexts(
+      searchResults?.html_content || ""
+    );
+    for (const searchWord of searchWords) {
+      const regex = new RegExp(`\\b${searchWord}\\b`, "gi");
+      const match = regex.exec(textToSearch);
+
+      if (match) {
+        const startWordIndex =
+          textToSearch.substring(0, match.index).split(/\s+/).length - 26;
+        const endWordIndex = startWordIndex + 40;
+        const words = textToSearch
+          .split(/\s+/)
+          .slice(startWordIndex, endWordIndex);
+        const matchedText = words.join(" ");
+        matchedTexts.push(matchedText);
+        if (matchedTexts.length) return matchedTexts.join("");
+      }
+    }
+  }
+
+  if (matchedTexts.length === 0) {
+    const words = textToSearch.split(/\s+/).slice(0, 50);
     return words.join(" ");
   }
 };
@@ -122,4 +140,51 @@ function convertToDate(data) {
   }
 
   return null;
+}
+
+const navigationTexts = [
+  "About Us",
+  "WHAT WE DO",
+  "WHERE WE ARE",
+  "OUR LEARNINGS",
+  "Where we are",
+  "Global Publications",
+  "Country Publications",
+  "Read more",
+  "Our locations",
+  "News center",
+  "Events",
+  "Newsletters",
+  "Join our community",
+  "Sign up to our newsletter",
+  "Report fraud, abuse, misconduct",
+  "Submit social or environmental complaint",
+  "Scam alert",
+  "Terms of use",
+  "United Nations Development Programme",
+  "Our work",
+  "Partnerships: the core of our network",
+  "UNTAPPED",
+  "Grassroots Energy Solutions",
+  "Blog",
+  "NEWS",
+  "twitter",
+  "linkedin",
+  "instagram",
+  "youtube",
+  "medium",
+  "© 2024 United Nations Development",
+];
+
+function removeNavigationTexts(searchText) {
+  const regex = new RegExp(
+    navigationTexts.map((text) => `\\b${text}\\b`).join("|"),
+    "gi"
+  );
+
+  // Replace occurrences of the texts with an empty string
+  return searchText
+    .replace(regex, "")
+    .replace(/\s{2,}/g, " ")
+    .trim();
 }
