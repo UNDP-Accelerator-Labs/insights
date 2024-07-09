45c03a12ddbb393274ac1f955b99382668da91c1
Adedapo Aderemi <adedapopaul@yahoo.com>
2024-07-09 11:48:41 +0100

    redirect browse to home

--- app.js
+++ app.js
@@ -69,7 +69,8 @@ const sessionMiddleware = session({
 app.use(sessionMiddleware);
 app.use(cookieParser(`${app_suite}-${app_suite_secret}-pass`));
 
-app.get("/", routes.home.index);
+// app.get("/", routes.home.index);
+app.get("/", routes.home.browse);
 app.get("/browse", routes.home.browse);
 app.get("/browse/toolkit", routes.browse.toolkit);
 
